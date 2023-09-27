c9a92fe1cd11ee716bfbd402707341e2471ba7d8
ddas <ddas@anl.gov>
2023-09-27 17:18:22 -0500

    updated the file name

---
+++ src/bsm-generator/start_BSM_Generator.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PYROOT=${PWD}/v2x_api_76201_lb
+export BUILD=debug
+
+export LIBROOT=${PYROOT}/${BUILD}/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIBROOT}
+export PYTHONPATH=${PYTHONPATH}:${PYROOT}/python/src
+
+PYEXE=$(which python3)
+
+${PYEXE} bsm-generator.py

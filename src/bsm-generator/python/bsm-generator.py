3fe06398c0698ce2e07b2e5696299d72c7effa85
ddas <ddas@anl.gov>
2024-01-18 13:47:54 -0600

    collected sample bsm for Andrew

--- src/bsm-generator/python/bsm-generator.py
+++ src/bsm-generator/python/bsm-generator.py
@@ -32,7 +32,7 @@ def main():
     
     # currentSpeed, currentTime = 10.1, time.time()
     bsmGenerator = BsmGenerator(config)
-    
+
     # bsmJsonString =  bsmGenerator.getBsmJsonString(currentSpeed, currentTime)
 
     while True:
@@ -49,14 +49,16 @@ def main():
             # print(type(bsmJsonString))
             # print("BSM Json is following:\n", bsmJsonString)
             encodedBsm = v2x.MessageFrame.from_json(bsmJsonString)
-            print(type(encodedBsm))
-            print(binascii.hexlify(encodedBsm))
+            hexlifyBsm = binascii.hexlify(encodedBsm)
+            # print(type(encodedBsm))
+            print(hexlifyBsm)
             # encodedBsm = binascii.hexlify(encodedBsm)
             print("Encoded BSM is Following:\n", encodedBsm)
 
             bsmGeneratorSocket.sendto(encodedBsm, clientAddress)
             
-    bsmGeneratorSocket.close()
 
+    bsmGeneratorSocket.close()
+    
 if __name__ == '__main__':
     main()

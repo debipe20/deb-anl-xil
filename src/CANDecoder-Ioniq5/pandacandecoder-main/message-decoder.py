9fd3ed77eccf6ee2523030a80327c46c918f6f51
debipe20 <debashisdas@arizona.edu>
2024-05-08 22:14:16 -0500

    interim commit

--- src/CANDecoder-Ioniq5/pandacandecoder-main/message-decoder.py
+++ src/CANDecoder-Ioniq5/pandacandecoder-main/message-decoder.py
@@ -32,7 +32,9 @@ Takes a path to a .csv file and creates signals based on that path. Columns for 
 A .csv file downloaded directly from VSpy3 will have a lot of extra information. The vspy_processing function in panda_preprocessing.py accepts a .csv file downloaded from VSpy3 and corrects the formatting. 
 It creates a new .csv file with 'processing_' prepended to the file name. By default, it deletes the original file, but setting remove=False allows the original file to remain.
 """
-all_data_path = 'can_data/processed_combined_data.csv'
+# all_data_path = 'can_data/processed_combined_data.csv'
+all_data_path = 'can_data/2023_Hyundai_Ioniq5_processed_dat.csv'
+
 
 for signal in known_signals:
     signal.set_data_path(all_data_path)

e804ced7155579558533673a519839d3b95ee246
ddas <ddas@anl.gov>
2024-05-22 13:30:59 -0500

    added ioniq5 signal and file path

--- src/CANDecoder-Ioniq5/pandacandecoder-main/message-decoder.py
+++ src/CANDecoder-Ioniq5/pandacandecoder-main/message-decoder.py
@@ -17,11 +17,11 @@ Choose scale and offset. Defaults to  1 and  0, respectively.
 
 # BYTES INDEXED STARTING AT 1
 # BITS INDEXED STARTING AT 0
-signals = [['0x245', '', (3, 3), (0, -1), 0.5, 0],
-           ['7DA', '04618a', (4, 5), (0, -1), 0.01, -327.68],
-           ['7DA', '0x06 61 81', (6, 7), (0, -1), 0.1, 0]]
+# signals = [['0x245', '', (3, 3), (0, -1), 0.5, 0],
+#            ['7DA', '04618a', (4, 5), (0, -1), 0.01, -327.68],
+#            ['7DA', '0x06 61 81', (6, 7), (0, -1), 0.1, 0]]
 # signals = [['7DA', '0x066181', (6, 7), (0, -1), 0.1, 0]]
-
+signals = [['7E4', '', (4,4),(0,-1),0.5,0]]
 known_signals = [Diagnostic(*signal) for signal in signals]
 first_bits_and_lengths = [signal.get_bits() for signal in known_signals]
 
@@ -32,8 +32,8 @@ Takes a path to a .csv file and creates signals based on that path. Columns for 
 A .csv file downloaded directly from VSpy3 will have a lot of extra information. The vspy_processing function in panda_preprocessing.py accepts a .csv file downloaded from VSpy3 and corrects the formatting. 
 It creates a new .csv file with 'processing_' prepended to the file name. By default, it deletes the original file, but setting remove=False allows the original file to remain.
 """
-all_data_path = 'can_data/processed_combined_data.csv'
-
+# all_data_path = 'can_data/processed_combined_data.csv'
+all_data_path = 'can_data/2023_Hyundai_Ioniq5_processed_dat.csv'
 for signal in known_signals:
     signal.set_data_path(all_data_path)
 

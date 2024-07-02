bef282aa8c96d7522904a45d225400ce3d30a03c
ddas <ddas@anl.gov>
2024-07-02 16:56:17 -0500

    modified codeline for adding test tab under flexil signal

--- src/web-application/app.py
+++ src/web-application/app.py
@@ -30,12 +30,19 @@ def form_contains_vehicle_signals(form_data):
             return True
     return False
 
-
 # Check if form data contains any Facilities signals
 def form_contains_facilities_signals(form_data):
     facilities_signals = load_config()['FlexILUDPSignals']['Facilities']
     for signal_name in facilities_signals:
         if f'Facilities_signal_{signal_name}' in form_data:
+            return True
+    return False
+
+# Check if form data contains any Test signals
+def form_contains_test_signals(form_data):
+    test_signals = load_config()['FlexILUDPSignals']['Test']
+    for signal_name in test_signals:
+        if f'Test_signal_{signal_name}' in form_data:
             return True
     return False
 
@@ -96,7 +103,18 @@ def update_config():
             # Update FlexILUDPSignals section for Facilities based on form data
             for signal_name in config_data['FlexILUDPSignals']['Facilities']:
                 if f'Facilities_signal_{signal_name}' in form_data:
-                    config_data['FlexILUDPSignals']['Facilities'][signal_name] = form_data.get(f'Facilities_signal_{signal_name}') == 'on'            
+                    config_data['FlexILUDPSignals']['Facilities'][signal_name] = form_data.get(f'Facilities_signal_{signal_name}') == 'on'   
+                    
+        # Check if form data contains Test signals
+        if form_contains_test_signals(form_data):
+            # Set all Test signals to False by default
+            for signal_name in config_data['FlexILUDPSignals']['Test']:
+                config_data['FlexILUDPSignals']['Test'][signal_name] = False
+
+            # Update FlexILUDPSignals section for Test based on form data
+            for signal_name in config_data['FlexILUDPSignals']['Test']:
+                if f'Test_signal_{signal_name}' in form_data:
+                    config_data['FlexILUDPSignals']['Test'][signal_name] = form_data.get(f'Test_signal_{signal_name}') == 'on'            
         
         # Save updated JSON data back to file
         save_config(config_data)

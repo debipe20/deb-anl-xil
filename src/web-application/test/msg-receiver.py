945725f8c667c452307f033da347550c77c4376f
debipe20 <debashisdas@arizona.edu>
2024-07-15 17:30:36 -0500

    tested communication with mabx hil setup

--- src/web-application/test/msg-receiver.py
+++ src/web-application/test/msg-receiver.py
@@ -1,10 +1,9 @@
 import socket
+import struct
 
-
-def main():
     
-hostIp = "192.168.26.150"
-port = 60001
+hostIp = "192.168.26.101"
+port = 1022
 hostAddress = (hostIp, port)
 
 testSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,9 +12,14 @@ testSocket.bind(hostAddress)
 
 while True:
     
-    data, address = testSocket.recvfrom(2048)
-    
-    print(data)
+    data, address = testSocket.recvfrom(80)
 
-if __name__ == "__main__":
-    main()
+    Dyno_Ctrl_Grade_Cmd, VUT_Ctrl_ACC_AxlTrq_Cmd, VUT_Ctrl_ACC_BrkDecel_Cmd, VUT_Ctrl_ACC_Accel_Cmd,  VUT_Ctrl_ACC_Accel_Actv,  VUT_Ctrl_Trns_Gear_Actv,  VUT_Ctrl_ACC_AxlTrq_Actv, VUT_Ctrl_LongCtrl_Method, VUT_Ctrl_ACC_BrkDecel_Actv, VUT_Ctrl_Trns_TrqCnvCl_Cmd = struct.unpack("dddddddddd", data)
+    
+
+    print(f"Decoded data of the following fields are ': {Dyno_Ctrl_Grade_Cmd, VUT_Ctrl_ACC_Accel_Cmd, VUT_Ctrl_Trns_TrqCnvCl_Cmd}")   
+
+    print(Dyno_Ctrl_Grade_Cmd)
+
+testSocket.close()
+

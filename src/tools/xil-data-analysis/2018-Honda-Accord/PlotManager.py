71ef6de5bae550a03f5e9273739fef4a8f0182ff
debipe20 <debashisdas@arizona.edu>
2024-10-14 11:13:41 -0500

    updated code to enhance the plot

--- src/tools/xil-data-analysis/2018-Honda-Accord/PlotManager.py
+++ src/tools/xil-data-analysis/2018-Honda-Accord/PlotManager.py
@@ -6,6 +6,7 @@ from nptdms import TdmsFile
 class PlotManager:
     def __init__(self, config):
         self.config = config
+        self.plot_save = config['PlotSave']
 
     def get_files(self):
         filePath = os.path.expanduser("~") + "/Downloads/2018-Honda-Accord/62409002 2018 Honda Accord Blank 1Bag 3600"
@@ -42,30 +43,33 @@ class PlotManager:
         self.speed_data = self.speed_channel[:]* 0.621371
         self.accel_data = self.accel_channel[:]
 
-    def plot_primary_yaxis(self, x_data, y_data, x_label, y_label, title):
+    def plot_primary_yaxis(self, x_data, y_data, x_label, y_label, title, fileName, plot_save_status):
 
         plt.plot(x_data, y_data, label="Speed")
         plt.title(title, fontsize=24, fontweight='bold')
         plt.xlabel(x_label, color='tab:green', fontsize=20)
         plt.ylabel(y_label, color='tab:blue', fontsize=20)
         plt.tick_params(axis='both', which='major', labelsize=16)
-        plt.legend(fontsize=16)
+        # plt.legend(fontsize=16)
         plt.grid(True)
-        plt.show()
+        
+        if plot_save_status:
+            plt.savefig(fileName +'.jpg', bbox_inches='tight', dpi=300)
+        
+        else:plt.show()
+        
         plt.close()
 
-    def plot_primary_secondary_yaxis(self, x_data, y_data1, y_data2, x_label, y_label1, y_label2, title):
+    def plot_primary_secondary_yaxis(self, x_data, y_data1, y_data2, x_label, y_label1, y_label2, title, fileName, plot_save_status):
         # # Create a figure and axis object
         fig, ax1 = plt.subplots()
 
-        # Plot speed (on primary y-axis)
         ax1.set_xlabel(x_label, color='tab:green', fontsize=20)
         ax1.set_ylabel(y_label1, color='tab:blue', fontsize=20)
         primary_axis_line, = ax1.plot(x_data, y_data1, color='tab:blue', label='Speed')
         ax1.tick_params(axis='y', labelcolor='tab:blue', labelsize=16)
         ax1.tick_params(axis='x', labelsize=16)  # Set label size for x-axis ticks
 
-        # Create a secondary y-axis to plot acceleration
         ax2 = ax1.twinx()  # Instantiate a second axes that shares the same x-axis
         ax2.set_ylabel(y_label2, color='tab:red', fontsize=20)
         secondary_axis_line, = ax2.plot(x_data, y_data2, color='tab:red', label='Acceleration')
@@ -75,25 +79,23 @@ class PlotManager:
         lines = [primary_axis_line, secondary_axis_line]  # Handles for both lines
         labels = [line.get_label() for line in lines]  # Labels for the lines
 
-        # Add the legend
-        ax1.legend(lines, labels, loc='upper right', fontsize=16)
-        # Add a title
+        # ax1.legend(lines, labels, loc='upper right', fontsize=16)
+        ax1.grid(True)
         plt.title(title, fontsize=24, fontweight='bold')
 
-        # Show grid
-        ax1.grid(True)
+        if plot_save_status:
+            plt.savefig(fileName +'.jpg', bbox_inches='tight', dpi=300)
 
-        # Show the  plot
-        plt.show()
-        # plt.savefig(fileName+'.jpg', bbox_inches='tight', dpi=72)
+        else:plt.show()
+
         plt.close(fig)
 
     def generate_plots(self):
         self.get_files()
         self.get_groups_channels()
         self.get_data_from_channel()
-        self.plot_primary_yaxis(self.time_data, self.speed_data, "Time [s]", "Speed [mph]", "Time vs. Speed Plot")
-        self.plot_primary_secondary_yaxis(self.time_data, self.speed_data, self.accel_data, "Time [s]", "Speed [mph]", "Acceleration [m/s²]", "Time vs. Speed and Acceleration Plot")
+        self.plot_primary_yaxis(self.time_data, self.speed_data, "Time [s]", "Speed [mph]", "Time vs. Speed Plot", "0-20_mph_time_vs_speed", self.plot_save)
+        self.plot_primary_secondary_yaxis(self.time_data, self.speed_data, self.accel_data, "Time [s]", "Speed [mph]", "Acceleration [m/s²]", "Time vs. Speed and Acceleration Plot", "0-20_mph_time_vs_speed_Accel", self.plot_save)
 
 
     

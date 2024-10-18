d62cdcf9cb3b7a635af00eec11f190e1610f478f
debipe20 <debashisdas@arizona.edu>
2024-10-18 11:35:05 -0500

    updated figures

--- src/tools/xil-data-analysis/2018-Honda-Accord/PlotManager.py
+++ src/tools/xil-data-analysis/2018-Honda-Accord/PlotManager.py
@@ -6,12 +6,16 @@ from nptdms import TdmsFile
 class PlotManager:
     def __init__(self, config):
         self.config = config
-        self.plot_save = config['PlotSave']
+        self.plot_save = self.config['PlotSave']
+        self.platform = self.config['Platform']
+        self.end_data_to_discard = self.config['NoOfEndDataDiscard']
 
     def get_files(self):
-        filePath = os.path.expanduser("~") + "/Downloads/2018-Honda-Accord/62409013 2018 Honda Accord Blank 1Bag 3600"
+        if self.platform == "Linux":
+            filePath = os.path.expanduser("~") + "/Downloads/2018-Honda-Accord/62409002 2018 Honda Accord Blank 1Bag 3600"
 
-        self.tdms_file = TdmsFile.read(filePath + "/62409013 Test Data.tdms")
+        else: filePath = "C:\\Users\ddas\\Documents\\Data\\2018-Honda-Accord\\62409002 2018 Honda Accord Blank 1Bag 3600"
+        self.tdms_file = TdmsFile.read(filePath + "\\62409002 Test Data.tdms")
 
     def get_groups_channels_name(self):
         # Get all groups in the TDMS file
@@ -39,23 +43,28 @@ class PlotManager:
 
     def get_data_from_channel(self):
    
-        self.time_data = self.time_channel[:]
-        self.speed_data = self.speed_channel[:]* 0.621371
-        self.accel_data = self.accel_channel[:]
+        self.time_data = self.time_channel[:-self.end_data_to_discard]
+        self.speed_data = self.speed_channel[:-self.end_data_to_discard]* 0.621371
+        self.accel_data = self.accel_channel[:-self.end_data_to_discard]
 
     def plot_primary_yaxis(self, x_data, y_data, x_label, y_label, title, fileName, plot_save_status):
 
         plt.plot(x_data, y_data, label="Speed")
-        plt.title(title, fontsize=24, fontweight='bold')
-        plt.xlabel(x_label, color='tab:green', fontsize=20)
-        plt.ylabel(y_label, color='tab:blue', fontsize=20)
-        plt.tick_params(axis='both', which='major', labelsize=16)
+        # plt.title(title, fontsize=18, fontweight='bold')
+        # plt.xlabel(x_label, color='tab:green', fontsize=16)
+        # plt.ylabel(y_label, color='tab:blue', fontsize=16)
+        # plt.tick_params(axis='both', which='major', labelsize=12)
         # plt.legend(fontsize=16)
+        plt.title(title, fontweight='bold')
+        plt.xlabel(x_label, color='tab:green')
+        plt.ylabel(y_label, color='tab:blue')
+        plt.tick_params(axis='both', which='major')
         plt.grid(True)
         
         if plot_save_status:
             file_directory = "figure/" + fileName + ".jpg"
             plt.savefig(file_directory, bbox_inches='tight', dpi=300)
+            print("saved file")
         
         else:plt.show()
         
@@ -65,16 +74,23 @@ class PlotManager:
         # # Create a figure and axis object
         fig, ax1 = plt.subplots()
 
-        ax1.set_xlabel(x_label, color='tab:green', fontsize=20)
-        ax1.set_ylabel(y_label1, color='tab:blue', fontsize=20)
+        # ax1.set_xlabel(x_label, color='tab:green', fontsize=16)
+        # ax1.set_ylabel(y_label1, color='tab:blue', fontsize=16)
+        # primary_axis_line, = ax1.plot(x_data, y_data1, color='tab:blue', label='Speed')
+        # ax1.tick_params(axis='y', labelcolor='tab:blue', labelsize=12)
+        # ax1.tick_params(axis='x', labelsize=12)  # Set label size for x-axis ticks
+        ax1.set_xlabel(x_label, color='tab:green')
+        ax1.set_ylabel(y_label1, color='tab:blue')
         primary_axis_line, = ax1.plot(x_data, y_data1, color='tab:blue', label='Speed')
-        ax1.tick_params(axis='y', labelcolor='tab:blue', labelsize=16)
-        ax1.tick_params(axis='x', labelsize=16)  # Set label size for x-axis ticks
+        ax1.tick_params(axis='y', labelcolor='tab:blue')
+
 
         ax2 = ax1.twinx()  # Instantiate a second axes that shares the same x-axis
-        ax2.set_ylabel(y_label2, color='tab:red', fontsize=20)
+        # ax2.set_ylabel(y_label2, color='tab:red', fontsize=16)
+        ax2.set_ylabel(y_label2, color='tab:red')
         secondary_axis_line, = ax2.plot(x_data, y_data2, color='tab:red', label='Acceleration')
-        ax2.tick_params(axis='y', labelcolor='tab:red', labelsize=16)
+        # ax2.tick_params(axis='y', labelcolor='tab:red', labelsize=12)
+        ax2.tick_params(axis='y', labelcolor='tab:red')
 
         # Combine legends from both axes
         lines = [primary_axis_line, secondary_axis_line]  # Handles for both lines
@@ -82,11 +98,13 @@ class PlotManager:
 
         # ax1.legend(lines, labels, loc='upper right', fontsize=16)
         ax1.grid(True)
-        plt.title(title, fontsize=24, fontweight='bold')
+        # plt.title(title, fontsize=18, fontweight='bold')
+        plt.title(title, fontweight='bold')
         
         if plot_save_status:
             file_directory = "figure/" + fileName + ".jpg"
             plt.savefig(file_directory, bbox_inches='tight', dpi=300)
+            print("saved file")
 
         else:plt.show()
 
@@ -97,8 +115,8 @@ class PlotManager:
         self.get_groups_channels()
         self.get_data_from_channel()
         # self.get_groups_channels_name()
-        # self.plot_primary_yaxis(self.time_data, self.speed_data, "Time [s]", "Speed [mph]", "Time vs. Speed Plot", "60_mph_time_vs_speed", self.plot_save)
-        # self.plot_primary_secondary_yaxis(self.time_data, self.speed_data, self.accel_data, "Time [s]", "Speed [mph]", "Acceleration [m/s²]", "Time vs. Speed and Acceleration Plot", "60_mph_time_vs_speed_Accel", self.plot_save)
+        self.plot_primary_yaxis(self.time_data, self.speed_data, "Time [s]", "Speed [mph]", "Time vs. Speed Plot", "0-20_mph_time_vs_speed", self.plot_save)
+        self.plot_primary_secondary_yaxis(self.time_data, self.speed_data, self.accel_data, "Time [s]", "Speed [mph]", "Acceleration [m/s²]", "Time vs. Speed and Acceleration Plot", "0-20_mph_time_vs_speed_Accel", self.plot_save)
 
 
     
